633da3e7049e73c0214c2c040b8d2c867b0e3422
Grace T <gtsai64@gmail.com>
2018-07-25 16:24:45 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -4,10 +4,10 @@
 # end
 # usd_to_eur(100)
 
-def usd_to_jpy(money)
-  puts "you have #{money * 110.95}"
-end
-usd_to_jpy(100)
+# def usd_to_jpy(money)
+#   puts "you have #{money * 110.95}"
+# end
+# usd_to_jpy(100)
 
 def usd_to_gbp(money)
     puts "you have #{money * 0.76}"
